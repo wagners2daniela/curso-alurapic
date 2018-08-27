e8e3d85a5771f9c662b66defb99b555f6740e87e
wagner <wagner.breackdance@gmail.com>
2018-08-26 23:25:44 -0300

    segmento de rotas

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -11,11 +12,16 @@ export class PhotoListComponent implements OnInit {
 
   photos: any[] = [];
 
-  constructor(private photoService: PhotoService) { }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
 
-    this.photoService.listFromUser('flavio')
+    const userName = this.activatedRoute.snapshot.params.userName;
+
+    this.photoService.listFromUser(userName)
       .subscribe(photos => this.photos = photos);
   }
 
