a846fc16be179307066bd758e8d3dda95d7c35b5
wagner <wagner.breackdance@gmail.com>
2018-08-27 00:21:59 -0300

    adicionado campo busca

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -11,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 export class PhotoListComponent implements OnInit {
 
   photos: any[] = [];
+  filter: string = '';
 
   constructor(
     private photoService: PhotoService,
