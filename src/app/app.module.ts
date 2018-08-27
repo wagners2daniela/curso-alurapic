9b09427beb7b7376e7309ef73ef9a43a63921825
wagner <wagner.breackdance@gmail.com>
2018-08-26 23:15:13 -0300

    criada a rota not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrosModule } from './erros/erros.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { AppRoutingModule } from './app.routing.module';
   imports: [
     BrowserModule,
     PhotosModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ErrosModule
 
   ],
   providers: [],
